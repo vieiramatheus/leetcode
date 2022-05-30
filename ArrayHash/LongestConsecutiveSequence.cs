40b21fe38bbeacf972012a2482a5139b962f0e84
matheus.vieira <matheus.vieira@bluecore.it>
2022-05-30 00:07:58 -0400

    adding the Longest Consecutive Sequence solution

--- ArrayHash/LongestConsecutiveSequence.cs
+++ ArrayHash/LongestConsecutiveSequence.cs
@@ -6,11 +6,97 @@ using System.Threading.Tasks;
 
 namespace Algoritmos.ArrayHash
 {
+    /// <summary>
+    /// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+    /// You must write an algorithm that runs in O(n) time.
+    /// </summary>
     public class LongestConsecutiveSequence
     {
+        /// <summary>
+        /// This is a solution that I invented, basically you store every number into a dictionary (hashmap)
+        /// Then you track sequences by doing a iteration that goes min -> max number
+        /// It works, but it is too slow
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public static int Solution1(int[] nums)
         {
+            var bucket = new Dictionary<int, int>();
 
+            int max = 0;
+            int min = int.MaxValue;
+            for (int index = 0; index < nums.Length; index++)
+            {
+                bucket.TryAdd(nums[index], nums[index]);
+                if (max < nums[index])
+                    max = nums[index];
+                if (min > nums[index])
+                    min = nums[index];
+            }
+
+            var sequence = 0;
+            var maxSequence = 0;
+            for (int index = min; index <= max; index++)
+            {
+                if (bucket.ContainsKey(index))
+                    sequence++;
+
+                else if (sequence > (nums.Length / 2))
+                    index = max;
+                else
+                    sequence = 0;
+
+                if (maxSequence < sequence)
+                    maxSequence = sequence;
+
+
+            }
+
+            return maxSequence;
+        }
+
+        /// <summary>
+        /// This is a solution that I invented, basically you store every number into a dictionary (hashmap)
+        /// Then you track sequences by doing a iteration that goes min -> max number
+        /// It works, but it is too slow
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int Solution2(int[] nums)
+        {
+            var bucket = new Dictionary<int, int>();
+
+            int max = 0;
+            int min = int.MaxValue;
+            for (int index = 0; index < nums.Length; index++)
+            {
+                bucket.TryAdd(nums[index], nums[index]);
+                if (max < nums[index])
+                    max = nums[index];
+                if (min > nums[index])
+                    min = nums[index];
+            }
+
+            int sequence = 0;
+            int sequenceIndex;
+            int maxSequence = 0;
+            for (int index = 0; index < nums.Length; index++)
+            {
+                if (!bucket.ContainsKey(nums[index] - 1))
+                {
+                    sequenceIndex = nums[index];
+                    while (bucket.ContainsKey(sequenceIndex))
+                    {
+                        sequence++;
+                        sequenceIndex++;
+                    }
+
+                    if (maxSequence < sequence)
+                        maxSequence = sequence;
+                }
+            }
+
+            return maxSequence;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,16 +6,21 @@ namespace Algoritmos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Write encoding stuff:");
-            var result1 = ArrayHash.EncodeDecodeStrings.Encode(new string[] { "lint", "code", "love", "you" });
-            Console.WriteLine(result1);
+            /// -> Length of the longest consecutive elements sequence.
+            var result = ArrayHash.LongestConsecutiveSequence.Solution1(new int[] { 9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6 });
+            Console.WriteLine(result);
 
-            Console.WriteLine("Write decoding stuff:");
-            var result2 = ArrayHash.EncodeDecodeStrings.Decode(result1);
-            foreach (var item in result2)
-            {
-                Console.WriteLine("{0}, ", item);
-            }
+            /// -> EncodeDecodeStrings
+            //Console.WriteLine("Write encoding stuff:");
+            //var result1 = ArrayHash.EncodeDecodeStrings.Encode(new string[] { "lint", "code", "love", "you" });
+            //Console.WriteLine(result1);
+
+            //Console.WriteLine("Write decoding stuff:");
+            //var result2 = ArrayHash.EncodeDecodeStrings.Decode(result1);
+            //foreach (var item in result2)
+            //{
+            //    Console.WriteLine("{0}, ", item);
+            //}
         }
     }
 }
